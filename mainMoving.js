42c3a4084f5fd3480722645bd65e934a996611da
kim0w <149936517+kim0w@users.noreply.github.com>
2023-11-05 19:15:09 +0900

    Add files via upload

---
+++ mainMoving.js
@@ -0,0 +1,11 @@
+const img = document.querySelector(".img");
+
+btn.addEventListener("click", clickHandler);
+
+function clickHandler(){
+  // ... 작업 로직 ...  //
+  
+  if(작업 성공){
+    window.location.href = "new\global-master\index.html";
+  }
+}
